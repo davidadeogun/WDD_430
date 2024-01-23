afb795c39597f37a0cfcb1adc1c62774332dc971
David Adeogun <adeogundavidt@gmail.com>
2024-01-23 18:08:27 -0500

    Refactored codes

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+// This is the loading state of the dashboard page. A very important concept Chapter 9
+export default function Loading() {
+    return <DashboardSkeleton />;
+}
