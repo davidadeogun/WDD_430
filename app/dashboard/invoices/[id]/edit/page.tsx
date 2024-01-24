e113766670ed92d3ad781ada1edc9d9106156f47
David Adeogun <adeogundavidt@gmail.com>
2024-01-24 10:43:00 -0500

    Add next-auth for authentication and updated metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,6 +3,12 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import {  fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { updateInvoice } from '@/app/lib/actions';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+  description: 'Edit Invoice page',
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
