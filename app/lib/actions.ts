f66a59da806b079ad5bce982015c28eea4f1bef8
David Adeogun <adeogundavidt@gmail.com>
2024-01-24 13:28:45 -0500

    Refactor authentication function in actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,6 +7,25 @@ import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -120,21 +139,4 @@ export async function createInvoice(prevState: State, formData: FormData) {
     }
   }
 
-  export async function authenticate(
-    prevState: string | undefined,
-    formData: FormData,
-  ) {
-    try {
-      await signIn('credentials', formData);
-    } catch (error) {
-      if (error instanceof AuthError) {
-        switch (error.type) {
-          case 'CredentialsSignin':
-            return 'Invalid credentials.';
-          default:
-            return 'Something went wrong.';
-        }
-      }
-      throw error;
-    }
-  }
+ 
